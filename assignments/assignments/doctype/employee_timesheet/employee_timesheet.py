96d7ad7fdf12713a7103ccb17d2e881f0ba45cae
sudhakutala <117260707+sudhakutala@users.noreply.github.com>
2024-10-30 08:29:10 +0000

    removing timesheet soltion changes from dev branch

--- assignments/assignments/doctype/employee_timesheet/employee_timesheet.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, Saisudha and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class EmployeeTimesheet(Document):
-	pass
